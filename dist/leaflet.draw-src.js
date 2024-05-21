fccbb7272b502cda6917488d49f4b22e9fdd79ad
Nazareno Rodriguez <nrodriguez-ctr@semantic-ai.com>
2024-05-21 11:47:31 -0300

    Rebuilding dist and updating .gitignore.

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1,5 +1,5 @@
 /*
- Leaflet.draw 1.0.3+9bf634d, a plugin that adds drawing and editing tools to Leaflet powered maps.
+ Leaflet.draw 1.0.3+5ea6fdf, a plugin that adds drawing and editing tools to Leaflet powered maps.
  (c) 2012-2017, Jacob Toye, Jon West, Smartrak, Leaflet
 
  https://github.com/Leaflet/Leaflet.draw
@@ -8,7 +8,7 @@
 (function (window, document, undefined) {/**
  * Leaflet.draw assumes that you have already included the Leaflet library.
  */
-L.drawVersion = "1.0.3+9bf634d";
+L.drawVersion = "1.0.3+5ea6fdf";
 /**
  * @class L.Draw
  * @aka Draw
